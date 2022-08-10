7bb8836b1cd7943a9751873ea695dcec36181021
w3villa-bedprakash <bedprakash.kushwaha@w3villa.com>
2022-08-10 19:18:39 +0530

    fetching from public api

--- assets/js/weatherInfo.js
+++ assets/js/weatherInfo.js
@@ -1,49 +1,61 @@
 const weatherInfo = function () {
-    this.finalData = {};
+    const APPID = "92d54516e130d926f9bdee04d6910339";
     displayWeather = function (data) {
-        console.log(data)
+        $('#description').html(data.weather);
+        $('#temp').html(data.temp);
+        $('#feels-like').html(data.temp);
+        $('#maxTemp').html(data.tempMax);
+        $('#minTemp').html(data.tempMin);
+        $('#humidity').html(data.humidity);
+        $('#wind').html(data.wind);
+        $('#county').html(data.country);
+        $('#sunrise').html(data.sunrise);
+        $('#sunset').html(data.sunset);
+        $('#icon').attr('src', data.icon);
     }
     this.getWeather = function (city) {
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=92d54516e130d926f9bdee04d6910339";
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APPID}`;
         $.ajax({
             url: url,
             dataType: "json",
             success: function (data) {
-                var weather = data.weather[0].description;
-                var temp = data.main.temp - 273.15;
-                var temp = Math.round(temp);
-                var temp = temp + "&deg;C";
-                var icon = data.weather[0].icon;
-                var icon = "http://openweathermap.org/img/w/" + icon + ".png";
-                var city = data.name;
-                var country = data.sys.country;
-                var humidity = data.main.humidity;
-                var humidity = humidity + "%";
-                var wind = data.wind.speed;
-                var wind = wind + "m/s";
-                var sunrise = data.sys.sunrise;
-                var sunrise = new Date(sunrise * 1000);
-                var sunrise = sunrise.toLocaleTimeString();
-                var sunset = data.sys.sunset;
-                var sunset = new Date(sunset * 1000);
-                var sunset = sunset.toLocaleTimeString();
-                var weatherInfo = {
-                    weather: weather,
-                    temp: temp,
-                    icon: icon,
-                    city: city,
-                    country: country,
-                    humidity: humidity,
-                    wind: wind,
-                    sunrise: sunrise,
-                    sunset: sunset
-                };
+                $("#city").removeClass("border-red");
+                const weatherInfo = {
+                    weather: data.weather[0].description,
+                    humidity: data.main.humidity,
+                    temp: Math.round(data.main.temp - 273.15) + "&deg,C",
+                    tempMax: Math.round(data.main.temp_max - 273.15) + "&deg,C",
+                    tempMin: Math.round(data.main.temp_min - 273.15) + "&deg,C",
+                    icon: "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
+                    city: data.name,
+                    country: data.sys.country,
+                    humidity: data.main.humidity + "%",
+                    wind: data.wind.speed + "m/s",
+                    sunrise: (new Date(data.sys.sunrise * 1000)).toLocaleTimeString(),
+                    sunset: (new Date(data.sys.sunset * 1000)).toLocaleTimeString()
+                }
                 displayWeather(weatherInfo)
+            },
+            error: function () {
+                $("#city").addClass("border-red");
+                alert("Invalid city!, Correct input format <City name, state code and country code divided by comma>");
             }
         });
     };
 }
 
+$(document).ready(function () {
+    const info = new weatherInfo();
+    //default location "Noida"
+    info.getWeather("Noida");
+})
 
-const info = new weatherInfo();
-info.getWeather("Noida");
+/**
+ * method to get weather info from public api and display it.
+ */
+function loadWeatherInfo() {
+    const city = $('#city').val()
+    //instantiate weatherInfo
+    const info = new weatherInfo();
+    info.getWeather(city);
+}
